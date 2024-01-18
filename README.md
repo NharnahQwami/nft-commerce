b7565edba568d4593daf9a0913703f4ccb8c2fc1
Desmond Dadzie <desdad9@gmail.com>
2024-01-18 17:21:05 +0000

    phase 1 complete

---
+++ README.md
@@ -0,0 +1,55 @@
+# eCommerce🛒
+
+This is a fully functional decentralized eCommerce website with a beautiful user interface and backend functionalities.
+
+## Features
+
+- User and guest checkout capabilities.
+- Setting up the templates and data structure in the first two modules.
+- Adding user checkout flow with payment integration.
+- After complete basic checkout with a logged-in user.
+- Add in the ability for users to checkout as a guest using cookies.
+
+## Technologies
+
+1. Django (Backend)
+2. Bootstrap (Frontend)
+3. CSS (Frontend)
+4. JavaScript (Client-Side)
+5. Paypal Integration (Payment Method)
+
+## To Run This Project
+
+1. clone this project on your local machine,
+
+```
+git clone
+```
+
+2. create a virtual environment inside eCommerce folder,
+
+```
+virtualenv venv
+```
+
+3. activate virtual environment,(for windows)
+
+```
+.\venv\Scripts\activate
+```
+
+4. install project dependencies from requirements.txt,
+
+```
+pip install -r requirements.txt
+```
+
+5. run project on your local machine,
+
+```
+python manage.py runserver
+```
+
+## Deployed
+
+👉 Live Demo: [e-Commerce](#)
---
+++ static/js/cart.js
@@ -0,0 +1,88 @@
+var updateBtns = document.getElementsByClassName('update-cart')
+
+for (i = 0; i < updateBtns.length; i++) {
+	updateBtns[i].addEventListener('click', function(){
+		var productId = this.dataset.product
+		var action = this.dataset.action
+		console.log('productId:', productId, 'Action:', action)
+		console.log('USER:', user)
+
+		if (user == 'AnonymousUser'){
+			addCookieItem(productId, action)
+		}else{
+			updateUserOrder(productId, action)
+		}
+	})
+}
+
+function addCookieItem(productId, action){
+	console.log('User is not authenticated')
+
+	if (action == 'add'){
+		if (cart[productId] == undefined){
+		cart[productId] = {'quantity':1}
+
+		}else{
+			cart[productId]['quantity'] += 1
+		}
+	}
+
+	if (action == 'remove'){
+		cart[productId]['quantity'] -= 1
+
+		if (cart[productId]['quantity'] <= 0){
+			console.log('Item should be deleted')
+			delete cart[productId];
+		}
+	}
+	console.log('CART:', cart)
+	document.cookie ='cart=' + JSON.stringify(cart) + ";domain=;path=/"
+
+	location.reload()
+}
+
+function updateUserOrder(productId, action){
+	console.log('User is authenticated, sending data...')
+
+		var url = '/update_item/'
+
+		fetch(url, {
+			method:'POST',
+			headers:{
+				'Content-Type':'application/json',
+				'X-CSRFToken':csrftoken,
+			},
+			body:JSON.stringify({'productId':productId, 'action':action})
+		})
+		.then((response) => {
+		   return response.json();
+		})
+		.then((data) => {
+		    location.reload()
+		});
+}
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
